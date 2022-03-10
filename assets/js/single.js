4e6105483e8e8fe90c799c8cdf614c573df36b06
Sip <sipg789@gmail.com>
2022-03-10 10:07:04 -0600

    added link

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,18 +1,6 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
 
-var displayWarning = function(repo) {
-    // add text to warning container 
-    limitWarningEl.textContent = "To see more than 30 issues, visit ";
-
-    var linkEl = document.createElement("a");
-    linkEl.textContent = "See More Issues on GitHub.com";
-    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
-    linkEl.setAttribute("target", "_blank");
-
-    // append to warning container 
-    limitWarningEl.appendChild(linkEl);
-};
 
 var getRepoIssues = function (repo) {
     console.log(repo);
@@ -27,7 +15,7 @@ var getRepoIssues = function (repo) {
 
                 // check if api has paginated issues 
                 if (response.headers.get("Link")) {
-                    displayWarning();
+                    displayWarning(repo);
                 }
             });
         }
@@ -74,5 +62,17 @@ var displayIssues = function (issues) {
     }
 };
 
+var displayWarning = function(repo) {
+    // add text to warning container 
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container 
+    limitWarningEl.appendChild(linkEl);
+};
 
 getRepoIssues("facebook/react");
